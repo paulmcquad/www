7f0421bdd3ebccc8eb9e1463194dfaa4507d2411
Paul McQuade <paulmcquad@gmail.com>
2017-05-16 14:46:56 +0100

    Added a Horizontal Navbar
    Basic Navbar
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ js/myslides.js
@@ -0,0 +1,14 @@
+/* SlidesJS Required: Initialize SlidesJS with a jQuery doc ready*/
+  $(function() {
+    $('#slides').slidesjs({
+      width: 940,
+      height: 528,
+      play: {
+        active: true,
+        auto: true,
+        interval: 4000,
+        swap: true
+      }
+    });
+  });
+/* End SlidesJS Required */
