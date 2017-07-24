15992cad948b367ff115f09aae485e37e1e61880
Paul McQuade <paulmcquad@gmail.com>
2017-07-24 19:19:15 +0100

    Create a footer
    remove bad code
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- index.php
+++ index.php
@@ -71,19 +71,7 @@
         </div>
     </div>
   </div>
+  <?php include('content/footer.php'); ?>
 
-      <div class="icon-bar">
-        <a href="https://www.facebook.com/paulmcquad"><i class="fa fa-facebook-square"></i></a>
-        <a href="https://www.linkedin.com/in/paulmcquad/"><i class="fa fa-linkedin-square"></i></a>
-        <a href="https://twitter.com/paulmcquad"><i class="fa fa-twitter-square"></i></a>
-        <a href="https://github.com/paulmcquad"><i class="fa fa-github-square"></i></a>
-        <a href="mailto:paulmcquad@gmail.com"><i class="fa fa-at"></i></a>
-      </div>
-
-
-  <!-- jQuery first, then Tether, then Bootstrap JS. -->
-  <script src="http://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </body>
 </html>
