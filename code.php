7a4275496ec9f7fd960ad29a9d6f6bf7893ce63f
Paul McQuade <paulmcquad@gmail.com>
2017-07-24 20:22:43 +0100

    Create a header file
    CSS files local/remote
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- code.php
+++ code.php
@@ -6,19 +6,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>Code</title>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<link rel="stylesheet" href="css/icon-font.css">
-<!-- container Grid -->
-<link rel="stylesheet" href="css/grid.css">
-
-<!-- <script src="js/jquery.slides.min.js"></script> -->
-
+  <?php include('content/header.php'); ?>
+  
 <body>
   <nav class="navbar navbar-inverse sidebar" role="navigation">
     <a class="navbar-brand" href="index.php">Paul McQuade</a>
